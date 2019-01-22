f8d9b67d12b5102b256a1ae73c2a05beee621646
Lucashsmello <lucashsmello@gmail.com>
2019-01-21 22:25:29 -0200

    Removed unnecessary files

--- startAP.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-systemctl daemon-reload
-systemctl restart hostapd
-systemctl restart dhcpcd
-systemctl restart dnsmasq
--- stopAP.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-systemctl stop hostapd
-systemctl stop dhcpcd
-systemctl stop dnsmasq
-sleep 1
